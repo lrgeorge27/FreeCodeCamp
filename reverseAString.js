5958b49595e5fe9eacf38bcd84cb02d53b9e9436
lrgeorge27 <lrgeorge27@gmail.com>
2017-11-07 17:53:05 +0000

    Reverse a String

---
+++ reverseAString.js
@@ -0,0 +1,15 @@
+function reverseString(str) {
+  var strArray = str.split("");
+//   console.log(strArray);
+  strArray.reverse();
+//   console.log("after reverse");
+//   console.log(strArray);
+  var reversedStr = strArray.join("");
+//   console.log("joining");
+//   console.log(reversedStr);
+  
+  return reversedStr;
+}
+
+console.log(reverseString("Lauren George"));
+
