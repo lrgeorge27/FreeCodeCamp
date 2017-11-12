f4bf72fc9a353ab17d85e04d37ea174da0d0facd
lrgeorge27 <lrgeorge27@gmail.com>
2017-11-12 19:15:18 +0000

    while loops

--- loops.js
+++ loops.js
@@ -1,3 +1,4 @@
+//Nesting For Loops
 function multiplyAll(arr) {
   var product = 1;
   // Only change code below this line
@@ -10,5 +11,16 @@ function multiplyAll(arr) {
   return product;
 }
 
+//Iterate While Loops
 // Modify values below to test your code
 multiplyAll([[1,2],[3,4],[5,4,7]]);
+
+// Setup
+var myArray = [];
+
+// Only change code below this line.
+var i = 0;
+while (i < 5) {
+  ourArray.push(i);
+  i++;
+}
