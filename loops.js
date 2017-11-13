10fbdcbd23780b9e9369293304e005df2737f3c9
lrgeorge27 <lrgeorge27@gmail.com>
2017-11-13 03:43:36 +0000

    finished loops and generate random numbers

--- loops.js
+++ loops.js
@@ -24,3 +24,89 @@ while (i < 5) {
   ourArray.push(i);
   i++;
 }
+
+//Profile Lookup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(firstName, prop){
+// Only change code below this line
+  for (var i = 0; i < contacts.length; i++) {
+  if (contacts[i].firstName === firstName) {
+    if (contacts[i].hasOwnProperty(prop)) {
+    return contacts[i][prop];
+  }
+  else {
+    return "No such property";
+    }
+  }
+}
+return "No such contact";
+// Only change code above this line
+}
+
+// Change these values to test your function
+lookUpProfile("Sherlock", "likes");
+
+
+//Generate random fractions
+function randomFraction() {
+
+  // Only change code below this line.
+
+  return Math.random();
+
+  // Only change code above this line.
+}
+
+//Generate Randon Whole Numbers
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+function randomWholeNum() {
+   return Math.floor(Math.random() * 10);
+}
+
+
+//Generate random whole #s w/in a range.
+// Example
+function ourRandomRange(ourMin, ourMax) {
+
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+ourRandomRange(1, 9);
+
+// Only change code below this line.
+
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random() * (myMax - myMin + 1)) + myMin; // Change this line
+
+}
+
+// Change these values to test your function
+var myRandom = randomRange(5, 15);
